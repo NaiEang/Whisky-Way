2d5e734df32c8ec3bc95c68cda0e2d38412762c2
phirum <sophaphirum2@gmail.com>
2025-07-10 12:00:08 +0700

    all file

--- res/tiles/map terrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="map terrain" tilewidth="32" tileheight="32" tilecount="4305" columns="41">
- <image source="../terrain/Overview.png" width="1312" height="3360"/>
-</tileset>
