2a3d681954abcdc421539d0aa98934fe2aac9006
Nai Eang <naieang016@gmail.com>
2025-08-22 05:54:42 +0700

    add road and parking space

---
+++ assets/tiles/whisky_way/road2.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="road2" tilewidth="16" tileheight="16" tilecount="384" columns="32">
+ <image source="roads2W-Photoroom.png" width="512" height="192"/>
+ <wangsets>
+  <wangset name="road" type="mixed" tile="289">
+   <wangcolor name="roadsurface" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="81" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="82" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="213" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="214" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="289" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="290" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="293" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="294" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="321" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="322" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="325" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="326" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
